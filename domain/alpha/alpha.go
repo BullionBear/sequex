57bad794b7a99d91902e01b9d5874f58ab551fc6
yite <yite@icloud.com>
2024-06-23 10:08:00 +0800

    momentum alpha

--- domain/alpha/alpha.go
+++ domain/alpha/alpha.go
@@ -1,12 +1,25 @@
 package alpha
 
+import (
+	"github.com/BullionBear/crypto-trade/api/gen/models"
+)
+
 type Alpha struct {
+	// 5 min moving average
+	ShortCloseMovingAvg *MovingAverage
+
+	// 1 hour term moving average
+	LongCloseMovingAvg *MovingAverage
 }
 
 func NewAlpha() *Alpha {
-	return &Alpha{}
+	return &Alpha{
+		ShortCloseMovingAvg: NewMovingAverage(300),
+		LongCloseMovingAvg:  NewMovingAverage(3600),
+	}
 }
 
-func (a *Alpha) GetConfig() {
-
+func (a *Alpha) Append(kline models.Kline) {
+	a.shortCloseMovingAvg.Append(kline.Close)
+	a.longCloseMovingAvg.Append(kline.Close)
 }
--- domain/models/models.go
+++ domain/models/models.go
@@ -1,4 +1,4 @@
-package model
+package models
 
 type Kline struct {
 	StartTime            int64
