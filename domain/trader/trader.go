b80df4d178333aa479cbb2b195576c27ff5bc95e
yite <yite@icloud.com>
2024-06-23 21:08:03 +0800

    extract backtest

--- domain/trader/trader.go
+++
@@ -1,8 +0,0 @@
-package trader
-
-import "github.com/shopspring/decimal"
-
-type Trader interface {
-	// Trade is the main function to make trading decisions.
-	CreateMarketOrder(symbol string, side bool, quoteQty decimal.Decimal) error // side: true for buy, false for sell
-}
