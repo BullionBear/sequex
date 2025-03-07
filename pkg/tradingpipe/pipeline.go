2a36c70af70ea82e1419f5c8124fddd45afa9679
yite <yite@icloud.com>
2025-03-07 19:56:05 +0800

    define trading pipeline method

--- pkg/tradingpipe/pipeline.go
+++ pkg/tradingpipe/pipeline.go
@@ -6,3 +6,12 @@ type TradingPipeline struct {
 func NewTradingPipeline() *TradingPipeline {
 	return &TradingPipeline{}
 }
+
+func (t *TradingPipeline) Run() {
+}
+
+func (t *TradingPipeline) Stop() {
+}
+
+func (t *TradingPipeline) Destroy() {
+}
