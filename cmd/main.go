7ef80da628b5a542a7149a16c8cf3122c8ad9c30
yite <coastq22889@icloud.com>
2025-03-14 21:39:07 +0800

    event driven system architect

--- cmd/main.go
+++ cmd/main.go
@@ -1,50 +1,60 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/BullionBear/sequex/internal/metadata"
 	"github.com/BullionBear/sequex/internal/strategy/sequex"
 	"github.com/BullionBear/sequex/internal/tradingpipe"
+	"github.com/BullionBear/sequex/pkg/message"
 	"github.com/BullionBear/sequex/pkg/mq/inprocq"
+	"github.com/google/uuid"
 )
 
 func main() {
 	// Resource
 	name := "Trading Pipeline"
 	strategy := sequex.NewSequex()
-	_ = tradingpipe.NewTradingPipeline(name, strategy)
-	_ = inprocq.New(8)
-	/*
-		ch, err := eventQ.Subscribe(name)
-		if err != nil {
-			panic(err)
+	pipeline := tradingpipe.NewTradingPipeline(name, strategy)
+	q := inprocq.NewInprocQueue()
+
+	go func() {
+		for {
+			msg := &message.Message{
+				ID:   uuid.New().String(),
+				Type: "kline_update",
+				Metadata: metadata.KLineUpdate{
+					Symbol:    "BTCUSDT",
+					Interval:  "1m",
+					Timestamp: time.Now().Unix(),
+				},
+			}
+			q.Send(msg)
+			time.Sleep(1 * time.Second)
 		}
-		go func() {
-			ticker := time.NewTicker(time.Second)
-			defer ticker.Stop()
-			for range ticker.C {
-				msg := message.Message{
-					ID:        uuid.New().String(),
-					Type:      "kline_update",
-					CreatedAt: time.Now().UnixMilli(),
-					Data:      nil,
-					Metadata: metadata.KLineUpdate{
-						Symbol:    "BTCUSDT",
-						Interval:  "1m",
-						Timestamp: time.Now().UnixMilli(),
-					},
+	}()
+
+	go func() {
+		for {
+			msg, err := q.RecvTimeout(2 * time.Second)
+			if err != nil {
+				fmt.Printf("Error receiving message: %v\n", err)
+			} else {
+				fmt.Printf("Received message: %v\n", msg)
+				switch msg.Type {
+				case "kline_update":
+					metadata := msg.Metadata.(metadata.KLineUpdate)
+					pipeline.OnKLineUpdate(metadata)
+
+				default:
+					fmt.Printf("Unknown message type: %v\n", msg.Type)
 				}
-				eventQ.Publish(name, msg)
-			}
-		}()
-		for event := range ch {
-			switch event.Type {
-			case "kline_update":
-				metadata := event.Metadata.(metadata.KLineUpdate)
-				tradingPipeline.OnKLineUpdate(metadata)
-			default:
-				fmt.Println("Unknown event type")
 			}
 		}
-		done := make(chan struct{})
-		<-done
-	*/
+	}()
+
+	done := make(chan struct{})
+	<-done
+
 }
