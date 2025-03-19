80e8a8571a595c9c77a6569ca28e3e3066033ba7
yite <yite@icloud.com>
2025-03-19 21:22:43 +0800

    solvexity method

--- internal/strategy/solvexity/solvexity.go
+++ internal/strategy/solvexity/solvexity.go
@@ -1,21 +1,39 @@
 package solvexity
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"time"
 
 	"github.com/BullionBear/sequex/internal/payload"
 	"github.com/BullionBear/sequex/internal/strategy"
+	pb "github.com/BullionBear/sequex/pkg/protobuf/solvexity"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var _ strategy.Strategy = (*Solvexity)(nil)
 
 type Solvexity struct {
+	c pb.SolvexityClient
 }
 
-func NewSolvexity() *Solvexity {
-	return &Solvexity{}
+func NewSolvexity(client pb.SolvexityClient) *Solvexity {
+	return &Solvexity{
+		c: client,
+	}
 }
 
-func (s *Solvexity) OnKLineUpdate(meta payload.KLineUpdate) {
-	fmt.Printf("solvexity strategy received kline update %+v\n", meta)
+func (s *Solvexity) OnKLineUpdate(meta payload.KLineUpdate) error {
+	ts := time.Unix(meta.Timestamp, 0).UTC()
+	resp, err := s.c.Solve(context.Background(), &pb.SolveRequest{
+		Symbol:    meta.Symbol,
+		Timestamp: timestamppb.New(ts),
+	})
+	if err != nil {
+		return err
+	}
+	if resp.GetStatus() != pb.StatusType_SUCCESS {
+		return errors.New(resp.GetMessage())
+	}
+	return nil
 }
--- internal/strategy/strategy.go
+++ internal/strategy/strategy.go
@@ -3,5 +3,5 @@ package strategy
 import "github.com/BullionBear/sequex/internal/payload"
 
 type Strategy interface {
-	OnKLineUpdate(payload.KLineUpdate)
+	OnKLineUpdate(payload.KLineUpdate) error
 }
