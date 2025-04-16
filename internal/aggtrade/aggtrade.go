7f784167777c4a1def0c12d6eeb78c5d3719de6c
yite <yite@icloud.com>
2025-04-16 15:27:39 +0800

    add agg trade

--- internal/aggtrade/aggtrade.go
+++ internal/aggtrade/aggtrade.go
@@ -1,9 +1,6 @@
 package aggtrade
 
 import (
-	"log"
-	"time"
-
 	"github.com/adshao/go-binance/v2"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
@@ -18,24 +15,4 @@ func NewAggTrade(client influxdb2.Client, limit int) *AggTrade {
 		client:    client,
 		eventChan: make(chan *binance.WsAggTradeEvent, limit),
 	}
-}
-
-func (a *AggTrade) Run(symbol string) error {
-	// Implement me
-	aggHandler := func(event *binance.WsAggTradeEvent) {
-		// Handle the aggregated trade event
-		if len(a.eventChan) < cap(a.eventChan) {
-			a.eventChan <- event
-		} else {
-			log.Printf("Event channel is full, dropping event")
-		}
-	}
-	doneC, stopC, err := binance.WsAggTradeServe(symbol, aggHandler, func(err error) {
-		log.Printf("error: %v", err)
-	})
-	ticker := time.NewTicker(1 * time.Second)
-	if err != nil {
-		return err
-	}
-	return nil
 }
