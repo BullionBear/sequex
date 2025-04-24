48a39b71a535e8683ea856ce937b0640a13f343f
yite <coastq22889@icloud.com>
2025-04-24 08:58:02 +0800

    cmd order client

---
+++ cmd/order/client/client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/BullionBear/sequex/pkg/protobuf/order"
+	pbdecimal "google.golang.org/genproto/googleapis/type/decimal"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewBinanceOrderServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.PlaceLimitOrder(ctx, &pb.LimitOrderRequest{
+		Symbol:   "BTCUSDT",
+		Side:     pb.Side_BUY,
+		Quantity: &pbdecimal.Decimal{Value: "0.01"},
+		Price:    &pbdecimal.Decimal{Value: "80000"},
+		Tif:      pb.TimeInForce_GTC,
+	})
+	if err != nil {
+		log.Fatalf("Error send limit order %v", err)
+	}
+	log.Printf("Order ID: %+v", r)
+}
