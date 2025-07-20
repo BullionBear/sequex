61881129158a4425acf6d977a543b3b5c1c9f376
yite <coastq22889@icloud.com>
2025-07-20 19:57:32 +0800

    Simplify main.go to basic Hello World example

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
-
-	"github.com/BullionBear/sequex/pkg/exchange/binance"
 )
 
 func main() {
-	c := binance.NewWSStreamClient(binance.DefaultConfig())
-	unsubscribe, _ := c.SubscribeToDiffDepthWithCallback("ETHUSDT", "@100ms", func(data *binance.WSDiffDepthData) error {
-		fmt.Println(data)
-		return nil
-	})
-	time.Sleep(10 * time.Second)
-	unsubscribe()
+	fmt.Println("Hello, World!")
 }
