1b5e5012a1f53215122cf93248c775381db3e86d
yite <yite@icloud.com>
2025-07-27 10:12:17 +0800

    init structure

--- internal/exchange/connector.go
+++ internal/exchange/connector.go
@@ -1,6 +1,6 @@
 package exchange
 
-type Connector interface {
+type SpotConnector interface {
 	GetBalance() (Balance, error)
 	GetOpenOrders() ([]Order, error)
 	GetOrder(orderID string) (Order, error)
@@ -16,5 +16,30 @@ type Connector interface {
 
 	GetOrderBook(symbol string, limit int) (OrderBook, error)
 	GetTrades(symbol string) ([]Trade, error)
-	GetPositions(symbol string) ([]Position, error)
+}
+
+type PerpConnector interface {
+	GetBalance() (Balance, error)
+	GetOpenOrders() ([]Order, error)
+	GetOrder(orderID string) (Order, error)
+	GetMyTrades(symbol string) ([]MyTrade, error)
+
+	CreateLimitOrder(symbol string, side string, price, quantity string, timeInForce string) (Order, error)
+	CreateLimitMakerOrder(symbol string, side string, price, quantity string) (Order, error)
+	CreateStopLimitOrder(symbol string, side string, price, quantity string, timeInForce string) (Order, error)
+	CreateMarketOrder(symbol string, side string, quantity string) (Order, error)
+
+	CancelOrder(orderID string) error
+	CancelAllOrders() error
+
+	GetOrderBook(symbol string, limit int) (OrderBook, error)
+	GetTrades(symbol string) ([]Trade, error)
+	GetPositions() ([]Position, error)
+}
+
+type UnifiedConnector interface {
+	GetBalance() (Balance, error)
+	GetOpenOrders() ([]Order, error)
+	GetOrder(orderID string) (Order, error)
+	GetMyTrades(symbol string) ([]MyTrade, error)
 }
