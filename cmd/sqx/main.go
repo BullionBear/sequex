3597b8b459af4775454edc25d7c257b4c213adcf
yitech <coastq22889@icloud.com>
2025-08-16 07:38:42 +0800

    update

--- cmd/sqx/main.go
+++ cmd/sqx/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"syscall"
@@ -16,10 +17,15 @@ import (
 	"github.com/BullionBear/sequex/pkg/node"
 	"github.com/BullionBear/sequex/pkg/shutdown"
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/proto"
+
+	pbCommon "github.com/BullionBear/sequex/internal/model/protobuf/common"
 )
 
 var (
 	logger     log.Logger
+	globalCfg  *config.GlobalConfig
+	err        error
 	configFile string
 )
 
@@ -31,6 +37,13 @@ func main() {
 		log.WithEncoder(log.NewTextEncoder()),
 	)
 
+	// Load global config to get NATS URL
+	globalCfg, err = config.LoadConfig[config.GlobalConfig](config.PathGlobalConfig)
+	if err != nil {
+		logger.Error("failed to load global config", log.Error(err))
+		os.Exit(1)
+	}
+
 	// Create root command
 	rootCmd := &cobra.Command{
 		Use:   "sqx",
@@ -40,8 +53,7 @@ or interact with other nodes as a client.
 
 Examples:
   sqx serve -c config.yml     # Start a server with config
-  sqx call rng --server localhost:8080 --input 10  # Call RNG service
-  sqx list --server localhost:8080                  # List available services`,
+  sqx call rng --server localhost:8080 --input 10  # Call RNG service`,
 		Version: fmt.Sprintf("Version: %s\nBuild Time: %s\nCommit Hash: %s",
 			env.Version, env.BuildTime, env.CommitHash),
 	}
@@ -61,33 +73,22 @@ Examples:
 
 	// Create client commands
 	callCmd := &cobra.Command{
-		Use:   "call [service]",
-		Short: "Call a specific service",
-		Long:  "Call a specific service on a remote node",
+		Use:   "call [method]",
+		Short: "Call a specific method",
+		Long:  "Call a specific method on a remote node",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return callService(args[0])
-		},
-	}
-
-	listCmd := &cobra.Command{
-		Use:   "list",
-		Short: "List available services",
-		Long:  "List all available services on a remote node",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return listServices()
+			method, _ := cmd.Flags().GetString("method")
+			return callService(args[0], method)
 		},
 	}
 
 	// Add flags for client commands
-	callCmd.Flags().String("server", "localhost:8080", "Server address to connect to")
-	callCmd.Flags().String("input", "", "Input data for the service")
-	listCmd.Flags().String("server", "localhost:8080", "Server address to connect to")
+	callCmd.Flags().String("method", "Supported methods: metadata, status, params", "Method to call")
 
 	// Add commands to root
 	rootCmd.AddCommand(serveCmd)
 	rootCmd.AddCommand(callCmd)
-	rootCmd.AddCommand(listCmd)
 
 	// Execute the root command
 	if err := rootCmd.Execute(); err != nil {
@@ -104,12 +105,6 @@ func runServer() error {
 		log.String("commit_hash", env.CommitHash),
 		log.String("config_file", configFile),
 	)
-
-	// Load global config
-	globalCfg, err := config.LoadConfig[config.GlobalConfig](config.PathGlobalConfig)
-	if err != nil {
-		return fmt.Errorf("failed to load global config: %w", err)
-	}
 
 	cfg, err := config.LoadConfig[node.NodeConfig](configFile)
 	if err != nil {
@@ -145,34 +140,120 @@ func runServer() error {
 }
 
 // callService calls a specific service on a remote node
-func callService(serviceName string) error {
-	// For now, use default values since we can't access flags from here
-	server := "localhost:8080"
-	input := ""
-
-	logger.Info("Calling service",
-		log.String("service", serviceName),
-		log.String("server", server),
-		log.String("input", input),
+func callService(serviceName, method string) error {
+	logger = log.New(
+		log.WithLevel(log.LevelInfo),
+		log.WithOutput(os.Stderr),
+		log.WithEncoder(log.NewTextEncoder()),
 	)
+	// Log only in debug mode or remove for cleaner output
+	// logger.Info("Calling service",
+	// 	log.String("service", serviceName),
+	// 	log.String("method", method),
+	// 	log.String("config_file", configFile),
+	// )
 
-	// TODO: Implement service call logic
-	logger.Info("TODO: Implement service call functionality")
+	// Load the node configuration to get RPC endpoints
+	cfg, err := config.LoadConfig[node.NodeConfig](configFile)
+	if err != nil {
+		return fmt.Errorf("failed to load node config: %w", err)
+	}
 
-	return nil
-}
+	// Connect to NATS
+	conn, err := nats.Connect(globalCfg.EventBus.Url)
+	if err != nil {
+		return fmt.Errorf("failed to connect to NATS: %w", err)
+	}
+	defer conn.Close()
 
-// listServices lists available services on a remote node
-func listServices() error {
-	// For now, use default values since we can't access flags from here
-	server := "localhost:8080"
+	eventBus := eventbus.NewEventBus(conn)
 
-	logger.Info("Listing services",
-		log.String("server", server),
-	)
+	// Map service names to RPC endpoints
+	var rpcEndpoint string
+	switch serviceName {
+	case "metadata":
+		rpcEndpoint = cfg.Rpc["req_metadata"]
+	case "status":
+		rpcEndpoint = cfg.Rpc["req_status"]
+	case "params":
+		rpcEndpoint = cfg.Rpc["req_parameters"]
+	default:
+		return fmt.Errorf("unknown service: %s. Supported services: metadata, status, params", serviceName)
+	}
 
-	// TODO: Implement service listing logic
-	logger.Info("TODO: Implement service listing functionality")
+	if rpcEndpoint == "" {
+		return fmt.Errorf("RPC endpoint not found for service: %s", serviceName)
+	}
+
+	// Create appropriate request based on service
+	var request proto.Message
+	var responseFactory func() proto.Message
+
+	switch serviceName {
+	case "metadata":
+		request = &pbCommon.MetadataRequest{Id: time.Now().UnixNano()}
+		responseFactory = func() proto.Message { return &pbCommon.MetadataResponse{} }
+	case "status":
+		request = &pbCommon.StatusRequest{Id: time.Now().UnixNano()}
+		responseFactory = func() proto.Message { return &pbCommon.StatusResponse{} }
+	case "params":
+		request = &pbCommon.ParametersRequest{Id: time.Now().UnixNano()}
+		responseFactory = func() proto.Message { return &pbCommon.ParametersResponse{} }
+	}
+
+	// Make RPC call
+	// logger.Info("Making RPC call",
+	// 	log.String("endpoint", rpcEndpoint),
+	// 	log.String("service", serviceName),
+	// )
+
+	response, err := eventBus.CallRPC(rpcEndpoint, request, responseFactory, time.Second)
+	if err != nil {
+		return fmt.Errorf("RPC call failed: %w", err)
+	}
+
+	// Handle response based on service type
+	switch serviceName {
+	case "metadata":
+		if resp, ok := response.(*pbCommon.MetadataResponse); ok {
+			if resp.Code != pbCommon.ErrorCode_ERROR_CODE_OK {
+				return fmt.Errorf("metadata request failed: %s", resp.Message)
+			}
+			metadataResult := map[string]interface{}{
+				"name":       resp.Name,
+				"created_at": resp.CreatedAt,
+				"emit":       resp.Emit,
+				"on":         resp.On,
+				"rpc":        resp.Rpc,
+			}
+			jsonData, _ := json.MarshalIndent(metadataResult, "", "  ")
+			fmt.Println(string(jsonData))
+		}
+	case "status":
+		if resp, ok := response.(*pbCommon.StatusResponse); ok {
+			if resp.Code != pbCommon.ErrorCode_ERROR_CODE_OK {
+				return fmt.Errorf("status request failed: %s", resp.Message)
+			}
+			var statusData map[string]interface{}
+			if err := json.Unmarshal(resp.Status, &statusData); err != nil {
+				return fmt.Errorf("failed to unmarshal status data: %w", err)
+			}
+			jsonData, _ := json.MarshalIndent(statusData, "", "  ")
+			fmt.Println(string(jsonData))
+		}
+	case "params":
+		if resp, ok := response.(*pbCommon.ParametersResponse); ok {
+			if resp.Code != pbCommon.ErrorCode_ERROR_CODE_OK {
+				return fmt.Errorf("parameters request failed: %s", resp.Message)
+			}
+			var paramsData map[string]interface{}
+			if err := json.Unmarshal(resp.Parameters, &paramsData); err != nil {
+				return fmt.Errorf("failed to unmarshal parameters data: %w", err)
+			}
+			jsonData, _ := json.MarshalIndent(paramsData, "", "  ")
+			fmt.Println(string(jsonData))
+		}
+	}
 
 	return nil
 }
