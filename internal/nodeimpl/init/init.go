4107f89816c116173f9f5fc1dc9dd32d6886c703
yitech <coastq22889@icloud.com>
2025-08-18 22:15:10 +0800

    register bar

--- internal/nodeimpl/init/init.go
+++ internal/nodeimpl/init/init.go
@@ -1,6 +1,7 @@
 package init
 
 import (
+	_ "github.com/BullionBear/sequex/internal/nodeimpl/app/bar"
 	_ "github.com/BullionBear/sequex/internal/nodeimpl/app/trade"   // Import to register Trade node
 	_ "github.com/BullionBear/sequex/internal/nodeimpl/example/rng" // Import to register RNG node
 	_ "github.com/BullionBear/sequex/internal/nodeimpl/example/sum" // Import to register Sum node
